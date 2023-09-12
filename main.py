3694e2da45c3c5cb3b15f7b020e8a3a255e944c6
Ahmed Hussein <wolktheline@yahoo.com>
2023-09-11 19:08:00 -0600

    Installing pandas and importing it into the man.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@
 # print(data)
 
 import csv
+import pandas
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-print(temperatures)
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+# print(temperatures)
